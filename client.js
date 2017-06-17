0113185590b93f609701abae17646dcc0f993ed2
helloworldzxy <zxy_bupt@sina.com>
2017-06-18 00:28:04 +0800

    add annotation

--- client.js
+++ client.js
@@ -14,14 +14,16 @@ function send(theName){
 		});
 		res.on('end', function(){
 			console.log('\n  \033[90m  request completed!\033[39m');
-			process.stdout.write('\n  your name: ');
+			process.stdout.write('\n  your name: '); //请求完毕后，打印出提示语，再次提示和等待用户发送数据
 		});
-	}).end(qs.stringify({name: theName}));
+	}).end(qs.stringify({name: theName})); //在end处发送数据
 }
 
 process.stdout.write('\n  your name: ');
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
+
+//获取用户的输入，并去除回车，作为要发送的内容，调用send函数发送HTTP request请求
 process.stdin.on('data', function(name){
-	send(name.replace('\n', ''));
+	send(name.replace('\n', '')); 
 });
